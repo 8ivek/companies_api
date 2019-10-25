7f14cacaad334e8eda5217c8fa6da85877d5927c
8ivek <bivek_j@yahoo.com>
2019-10-25 19:32:24 -0400

    CORS added

--- public/index.php
+++ public/index.php
@@ -7,6 +7,11 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+// 8ivek: for cors, remove following 2 lines in production env
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 define('LARAVEL_START', microtime(true));
 
 /*
